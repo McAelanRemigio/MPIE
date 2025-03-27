febdb7ebfb87e4bf5a6f68ac86e01c9b174ec26a
McAelan Remigio <143043961+McAelanRemigio@users.noreply.github.com>
2025-03-26 20:54:09 -0700

    by_term.R

--- by_term.R
+++ by_term.R
@@ -8,24 +8,21 @@ library(scales)
 View(tidy_data)
 
 # What are the pass rates for each term?
-data = data %>%
-  mutate(pass = ifelse(Grade %in% c("A", "B", "C", "P"), 1, 0))
-
-data = data %>% mutate(course_name = sub("-[A-Za-z0-9]+$", "", data$course_name))
-
-passrate = data %>% mutate(pass = ifelse(Grade %in% c ("A", "B", "C", "P"), 1, 0)) %>%
-  group_by(course_name) %>%
+terms_passrate = data %>% 
+  mutate(pass = ifelse(Grade %in% c ("A", "B", "C", "P"), 1, 0)) %>% 
+  group_by(EnrollmentTerm) %>%
   summarise(
-    total = n(),
-    pass= sum(pass),
-    passrate = (pass / total) * 100
+    total_students = n(),
+    total_passed = sum(pass),
+    pass_rate = total_passed / total_students
   )
-passrate
-# View(passrate)
 
-ggplot(passrate, aes(x = reorder(course_name, passrate), y = passrate)) +
-  geom_col(fill = "steelblue") +
-  coord_flip() +
-  labs(title = "Pass Rates by Course", x = "Course Name", y = "Pass Rate (%)") +
+terms_passrate
+# View(terms_passrate)
+
+ggplot(terms_passrate, aes(x = EnrollmentTerm, y = pass_rate * 100, group = 1)) +
+  geom_line(color = "steelblue", size = 1) +
+  geom_point(color = "red", size = 2) +
+  labs(title = "Pass Rate Trends Over Time", x = "Enrollment Term", y = "Pass Rate (%)") +
   ylim(0, 100) +
   theme_minimal()
